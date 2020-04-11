78f0bbdd33d2bbfb953721030d51b515b3b74545
combattardigrade <combattardigrade@gmail.com>
2020-04-11 09:11:11 -0500

    server changes

--- app_api/controllers/transaction.js
+++ app_api/controllers/transaction.js
@@ -140,7 +140,7 @@ module.exports.getRankingsByPeriod = (req, res) => {
                 'id', ['toUserId', 'userId'],
                 [sequelize.fn('sum', sequelize.col('amount')), 'total_amount']
             ],
-            group: ['userId'],
+            group: ['toUserId'],
             limit: 50,
             raw: true,
             transaction: t
